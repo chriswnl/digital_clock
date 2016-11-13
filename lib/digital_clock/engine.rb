5d073c47cb2c1e8b0bfc5ffc14059c068585bc28
Chris Willis <chris@twigtelecom.nl>
2016-11-13 21:52:52 +0100

    moving assets

--- lib/digital_clock/engine.rb
+++ lib/digital_clock/engine.rb
@@ -1,7 +1,7 @@
 module DigitalClock
   class Engine < ::Rails::Engine
     initializer "digital_clock.assets" do
-      ::Rails.application.config.assets.precompile += %w( moment.js )
+      ::Rails.application.config.assets.precompile += %w( moment.js digital_clock.js digital_clock.css )
     end
   end
 end
